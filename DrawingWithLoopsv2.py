30d17c5c049fd03f4bb8371b4901909742ccb18e
OM3G424747 <44314634+OM3G424747@users.noreply.github.com>
2020-12-29 02:20:14 +0200

    Add files via upload
    Text art created with loops in python for fun :)

---
+++ DrawingWithLoopsv2.py
@@ -0,0 +1,28 @@
+"""
+This file was created to print an ascii text art heart using loops.
+ ... For shits and giggles
+"""
+
+
+
+Print_Left_Blank = " "
+Print_Center_Blank = " "
+Print_Heart = "X"
+Blank_Length = 10
+Heart_Lenght = 6
+
+for pos in range (1,6):
+    print(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}{(Print_Center_Blank*Blank_Length)}{(Print_Heart*Heart_Lenght)}")
+    Blank_Length = Blank_Length -2
+    Heart_Lenght = Heart_Lenght +3
+    if Heart_Lenght == 18:
+        for pos in range (1,3):
+            print(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}{(Print_Center_Blank*Blank_Length)}{(Print_Heart*Heart_Lenght)}")
+
+Heart_Lenght = 42
+Blank_Length = 0
+
+for pos in range(1,11):
+    Blank_Length = Blank_Length +2
+    Heart_Lenght = Heart_Lenght -4
+    print(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}")
