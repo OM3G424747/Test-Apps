0a14939473bc2aa7794e9215bc2abc47fee20c40
OM3G424747 <44314634+OM3G424747@users.noreply.github.com>
2021-01-03 06:25:11 +0200

    Update DrawingWithLoopsv2.py
    updated to create an output file with a copy of the ascii heart

--- DrawingWithLoopsv2.py
+++ DrawingWithLoopsv2.py
@@ -1,6 +1,6 @@
 """
 This file was created to print an ascii text art heart using loops.
- ... For shits and giggles
+This file also outputs a txt file called "HeartPrint" that as a copy of the ascii heart
 """
 
 
@@ -10,14 +10,17 @@ Print_Center_Blank = " "
 Print_Heart = "X"
 Blank_Length = 10
 Heart_Lenght = 6
+output = open("HeartPrint.txt","w")
 
 for pos in range (1,6):
     print(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}{(Print_Center_Blank*Blank_Length)}{(Print_Heart*Heart_Lenght)}")
+    output.write(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}{(Print_Center_Blank*Blank_Length)}{(Print_Heart*Heart_Lenght)}\n")
     Blank_Length = Blank_Length -2
     Heart_Lenght = Heart_Lenght +3
     if Heart_Lenght == 18:
         for pos in range (1,3):
             print(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}{(Print_Center_Blank*Blank_Length)}{(Print_Heart*Heart_Lenght)}")
+            output.write(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}{(Print_Center_Blank*Blank_Length)}{(Print_Heart*Heart_Lenght)}\n")
 
 Heart_Lenght = 42
 Blank_Length = 0
@@ -26,3 +29,6 @@ for pos in range(1,11):
     Blank_Length = Blank_Length +2
     Heart_Lenght = Heart_Lenght -4
     print(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}")
+    output.write(f"{(Print_Left_Blank)*Blank_Length}{(Print_Heart)*Heart_Lenght}\n")
+
+output.close()
